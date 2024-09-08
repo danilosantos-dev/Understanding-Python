cdc440db1da365e29a081e6455a269110f62eb4c
Danilo Santos <santosdanilo2024@gmail.com>
2024-09-08 17:58:15 -0300

    resolvendo desafio

--- Desafio/adivinhe.py
+++ Desafio/adivinhe.py
@@ -1,3 +1,22 @@
+import random
+
 #Crie um sistema que o usurio tenha que adivinhar um número entre
 #1 e 10, ele terá apenas três chances para acertar!
 
+print("Adivinhe o número que esta entre 1 e 10!")
+randomNum = random.randint(1,10)
+
+responseQuant = 0
+while responseQuant != 3:
+    
+    responseUser = int(input("Sua resposta:"))
+
+    if responseUser == randomNum:
+        print("Parabéns, você acertou!")
+        break    
+    else: 
+        responseQuant += 1 
+    
+    
+    
+    
